3f999cfd7690a1f283d71377c70194cb9c986993
t-adekunle <tayoadekunle97@gmail.com>
2023-03-18 16:49:46 +0000

    add plots and define ratings

--- Founders and Coders/moviedata.js
+++ Founders and Coders/moviedata.js
@@ -34,5 +34,50 @@ let movieData = {
     },
   };
 
-  console.log(movieData)
-  console.log(moviesData[1])
+  console.log(movieData);
+  console.log(Object.keys(movieData));
+  console.log(Object.keys(movieData)[0]);
+
+const plots = document.getElementsByClassName('plot');
+
+
+const firstPlot = plots[0];
+firstPlot.textContent = "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.";
+
+const secondPlot = plots[1];
+secondPlot.textContent = "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons.";
+
+const thirdPlot = plots [2];
+thirdPlot.textContent = "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.";
+
+const fourthPlot = plots [3];
+fourthPlot.textContent = "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.";
+
+const ratings = document.getElementsByClassName('rating');
+
+const firstRating = ratings[0];
+firstRating.textContent = "7.2"
+
+const secondRating = ratings[1];
+
+const thirdRating = ratings[2];
+
+const fourthRating = ratings[3];
+
+
+
+
+
+
+
+
+
+
+
+  //const object1 = {
+ // a: 'somestring',
+//  b: 42,
+ // c: false
+//};
+
+//console.log(Object.keys(object1)[0]); logs "a" onto the console.
