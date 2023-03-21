30c9e2774c04f706b9fdec8fc8f99f37fe8c3b39
t-adekunle <tayoadekunle97@gmail.com>
2023-03-21 12:19:36 +0000

    fix sizing of boxes, add collapsible element on all, add box backgrounds

--- Founders and Coders/moviedata.js
+++ Founders and Coders/moviedata.js
@@ -129,6 +129,21 @@ let filmCast4 = movieData["The Grand Budapest Hotel"].cast;
 formattedFilmCast4 = filmCast4.join(', ');
 fourthCast.textContent = " " + formattedFilmCast4;
 
+var coll = document.getElementsByClassName('collapse');
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        var collapse = this.nextElementSibling;
+        if (collapse.style.display === "grid") {
+            collapse.style.display = "none";
+        }
+        else {
+            collapse.style.display = "grid";
+        }
+    });
+    }
+
 
 
 
